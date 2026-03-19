36b5c4cf6958bd0705ef7cf37d75acd9034c1561
Baxpk77 <Big2axe@gmail.com>
2026-03-19 13:55:12 +0100

    Création du fichier jeu principal, système de pioche : affichage et génération de la pioche

--- game.py
+++ game.py
@@ -2,13 +2,24 @@ from piece import Piece
 from plateau import Plateau
 
 class Game:
-    def __init__(self, x:int, y:int):
+    def __init__(self, x=4, y=4):
+        self.generer_pioche()
+        self.afficher_pioche()
 
     def generer_pioche(self):
-        self.pioche = []
+        "Génère la pioche du jeu"
+        self.pioche = {}
 
         for i in range(2):
             for j in range(2):
                 for k in range(2):
-                    for l un range(2):
-                        pioche += [Piece(i,j,k,l)]
+                    for l in range(2):
+                        self.pioche[i,j,k,l] = Piece(i,j,k,l)
+    
+    def afficher_pioche(self):
+        print("PIOCHE ⛏️\n"+"-"*70)
+        for key in self.pioche.keys():
+            print(f"{key} : {self.pioche[key]}")
+
+
+game = Game()
