b12628e06490fc73dac253b660cca06913ee9431
Baxpk77 <Big2axe@gmail.com>
2026-03-19 15:02:25 +0100

    tests de comp()

--- piece.py
+++ piece.py
@@ -62,4 +62,10 @@ def test():
      p2 = Piece(1,0,1,0)
      p3 = Piece(1,0,0,0)
      p4 = Piece(1,1,0,1)
-     print(comp([p1,p2,p3,p4]))
+     p5 = Piece(0,0,0,0)
+     assert(comp([p1,p2,p3,p4])==True)
+     assert(comp([p1,p5,p3,p4])==False)
+     assert(comp([p5,p3,p4])==True)
+
+
+test()
