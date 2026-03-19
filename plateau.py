5c6b99d8db21b88beb1a5ca62b2221e11f2150be
mrinuccini <mrinuccini>
2026-03-19 13:00:43 +0100

    Plateau

--- plateau.py
+++ plateau.py
@@ -1,2 +1,20 @@
+from piece import Piece
+
 class Plateau:
-    pass
+    arr = []
+    x = 0
+    y = 0
+
+    def __init__(self, x: int, y: int) -> None:
+        assert(x > 0)
+        assert(y > 0)
+
+        self.arr = [[Piece(0, 0, 0, 0) for i in range(o, x)] for i in range(0, y)]
+        self.x = x
+        self.y = y
+
+    def recuperer_piece(self, x:int, y: int) -> Piece:
+        assert(0 <= x <= self.x)
+        assert(0 <= y <= self.y)
+
+        return arr[y, x]
