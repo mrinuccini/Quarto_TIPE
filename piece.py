e36d51d83a0db113e5cd08eed274e176c8e45490
Baxpk77 <Big2axe@gmail.com>
2026-03-19 12:59:29 +0100

    Défintiion de pièce de base

--- piece.py
+++ piece.py
@@ -3,5 +3,8 @@ class Piece:
     def __init__(self, couleur, forme, dessus, taille):
         """Paramètres :
                 couleur : 0 ou 1, couleur de la pièce (0=blanc, 1=noir)
+                forme : 0 ou 1, forme de la pièce (0=carré, 1=rond)
+                dessus : 0 ou 1, dessus de la pièce (0=lisse, 1=creusé)
+                taille : 0 ou 1, taille de la pièce (0=petite, 1=grande)
         """
         self.couleur = couleur
