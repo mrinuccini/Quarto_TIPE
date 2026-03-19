2197dfab74ed346ef369e4f1bc9f92d157f27027
Baxpk77 <Big2axe@gmail.com>
2026-03-19 13:02:05 +0100

    modif peice

--- piece.py
+++ piece.py
@@ -7,4 +7,8 @@ class Piece:
                 dessus : 0 ou 1, dessus de la pièce (0=lisse, 1=creusé)
                 taille : 0 ou 1, taille de la pièce (0=petite, 1=grande)
         """
+        assert(couleur in (0,1))
+        assert(forme in (0,1))
+        assert(dessus in (0,1))
+        assert(taille in (0,1))
         self.couleur = couleur
