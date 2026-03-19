8228e510c5b945d479993f35d7bf29d5ec33d4bd
mrinuccini <mrinuccini>
2026-03-19 13:30:14 +0100

    Possibilité d'ajouter des pièces sur le plateau

--- plateau.py
+++ plateau.py
@@ -27,15 +27,23 @@ class Plateau:
 
     def placer_piece(self, x: int, y: int, piece: Piece) -> None:
         """
-            
+            Permet de placer une pièce à la position (x, y) sur le plateau
         """
+        assert(0 <= x < self.x)
+        assert(0 <= y < self.y)
+
+        if self.arr[y][x] != None:
+            print(f"Impossible de placer une pièce à la position (x={x}; y={y}).")
+            return
+
+        self.arr[y][x] = piece
 
     def __repr__(self) -> str:
         out = ""
         for y in range(0, self.y):
             for x in range(0, self.x):
                 out += repr(self.arr[y][x])
-                out += " "
+                out += " | "
             out += "\n"
 
         return out
--- test_tableau.py
+++ test_tableau.py
@@ -1,7 +1,16 @@
 from plateau import Plateau
+from piece import Piece
 
 if __name__ == "__main__":
     plateau = Plateau(4, 4)
-    
-    print(plateau)
+
+    print(plateau) # Grille 4x4 avec des None
+
+    plateau.placer_piece(1, 0, Piece(1, 0, 0, 0))
+    print(plateau) # Grille 4x4 avec des Nones et une pièce noire carrée lisse petite en (1, 0)
+
+    plateau.placer_piece(1, 0, Piece(1, 0, 0, 0))
+    print(plateau) # Grille 4x4 avec des Nones et une pièce noire carrée lisse petite en (1, 0)
+
+
 
