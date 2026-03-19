c43776e7a7c19a86cef0d47afbcf4f6582507397
Baxpk77 <Big2axe@gmail.com>
2026-03-19 15:16:15 +0100

    tests de convert_matrice

--- toolbox.py
+++ toolbox.py
@@ -1,8 +1,14 @@
 def convert_matrice(M):
+    """Intervertit les lignes et les colonnes de la matrice M"""
+    #Assertions
     assert(type(M)==list)
     assert(M!=[])
     assert(type(M[0])==list)
 
+    #Matrice vide
+    if M==[[]]:
+        return [[]]
+    
     n = len(M)
     p = len(M[0])
     M2 = []
@@ -12,4 +18,16 @@ def convert_matrice(M):
         for i in range(n):
             line += [M[i][j]]
         M2 += [line]
+
     return M2
+
+
+def tests():
+    M = [
+        [1,2,3],
+        [4,5,6]
+    ]
+    assert(convert_matrice(M)==[[1,4], [2,5], [3,6]])
+    assert(convert_matrice(convert_matrice(M))==M)
+
+tests()
