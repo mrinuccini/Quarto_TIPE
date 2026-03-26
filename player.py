11cb4ec453e37e4477f2c401583272cd8ea73f00
Baxpk77 <Big2axe@gmail.com>
2026-03-26 18:24:56 +0100

    rajout des fonctions choisir_piece et choisir_place dans la classe player

--- player.py
+++ player.py
@@ -3,10 +3,21 @@ TYPES = ["Humain", "Monte Carlo", "MinMax"]
 
 
 class Joueur:
-    def __init__(self, type="Humain", niveau=1):
+    def __init__(self, typ="Humain", niveau=1):
         """ Paramètre :
                 type : chaîne de caractères, élément de TYPES
                        type de joueur
                 niveau : entier naturel
                          niveau de l'IA
         """
+        assert(typ in TYPES)
+        self.type = typ
+        self.niveau = niveau
+
+    def choisir_piece(self, plateau, pioche):
+        """ Choix d'une pièce que devra placer le joueur suivant, selon le type du joueur """
+        pass
+
+    def choisir_place(self, plateau, pioche, piece):
+        """ Choix du placement de la pièce selon le type du joueur """
+        pass
