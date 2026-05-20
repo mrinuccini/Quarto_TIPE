291c46756c78741930da08f21a1dede52dfadc1d
Baxpk77 <big2axe@gmail.com>
2026-05-20 11:18:29 +0200

    Corrections de bugs, codes plus lisible

--- Tree.py
+++ Tree.py
@@ -1,6 +1,7 @@
 "Instanciaiton d'arbres"
 from Queue import *
 from math import log, sqrt
+from move import *
 
 class Node:
     "Instanciation d'un nœud"
@@ -86,15 +87,40 @@ class Node_MCTS(Node):
         self.win = 0 #Nombre de victoires associé au noeud
         self.visited = 0 #Nombre de visites du noeud
         self.move = move #mouvement associé au noeud
-        self.untried_move = None
+        self.untried_moves = None #Mouvement non essayés
         
     def get_win_number(self):
         "Renvoie le nombre de victoires associé au noeud"
         return self.win
         
+    def increase_win_number(self, val):
+        "Augmente le nombre de victoires associé au noeud"
+        self.win += val
+
     def get_visit_number(self):
         "Renvoie le nombre de visites associé au noeud"
-        return self.win
+        return self.visited
+    
+    def increase_visit_number(self):
+        "Augmente le nombre de visite du noeud"
+        self.visited += 1
+        
+    def get_parent(self):
+        "Renvoie le parent du noeud"
+        return self.parent
+        
+    def get_move(self):
+        "Renvoie le mouvement associé au noeud"
+        return self.move
+    
+    def gen_untried_moves(self, cases, pieces):
+        "Génère les mouvements non essayés selon la liste de cases cases et celle de pieces pieces"
+        self.untried_moves = [Move(c, p) for c in cases for p in pieces]
+
+
+    def get_untried_moves(self):
+        "Renvoie les mouvements non essayés"
+        return self.untried_moves
 
 
     def insert(self, node):
